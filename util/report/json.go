7b252e6fdcd6c2d84ea182f3b86188959ec94c30
luotianqi777 <luotianqi777@gmail.com>
2022-05-30 15:17:19 +0800

    fix error of json report is empty

--- util/report/json.go
+++ util/report/json.go
@@ -9,6 +9,9 @@ import (
 // Json 获取json格式报告数据
 func Json(dep *model.DepTree, taskInfo TaskInfo) []byte {
 	format(dep)
+	if taskInfo.Error != nil {
+		taskInfo.ErrorString = taskInfo.Error.Error()
+	}
 	if data, err := json.Marshal(struct {
 		*model.DepTree
 		TaskInfo TaskInfo `json:"task_info"`
