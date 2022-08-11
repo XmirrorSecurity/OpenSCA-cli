3996d9290655865efc70e209f9b20dc030da30a7
luotianqi777 <luotianqi777@gmail.com>
2022-08-11 15:16:12 +0800

    fix invalid zip file error

--- analyzer/engine/archive.go
+++ analyzer/engine/archive.go
@@ -8,7 +8,10 @@ package engine
 import (
 	"archive/tar"
 	"archive/zip"
+	"bufio"
+	"bytes"
 	"compress/gzip"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
@@ -36,84 +39,138 @@ func (e Engine) checkFile(filename string) bool {
 	return false
 }
 
+// checkArchive 校验压缩包格式
+func checkArchive(filepath string) {
+	// 校验Zip包格式
+	if filter.Zip(filepath) || filter.Jar(filepath) {
+		f, err := os.Open(filepath)
+		if err != nil {
+			logs.Warn(err)
+			return
+		}
+		defer f.Close()
+		r := bufio.NewReader(f)
+		head, err := r.Peek(4)
+		if err != nil {
+			logs.Warn(err)
+			return
+		}
+		magic := []byte{0x50, 0x4B, 0x03, 0x04}
+		ok := true
+		for i := range head {
+			if head[i] != magic[i] {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			return
+		}
+		data, err := os.ReadFile(filepath)
+		if err != nil {
+			logs.Warn(err)
+		}
+		index := bytes.Index(data, magic)
+		if index > 0 {
+			os.WriteFile(filepath, data[index:], 0766)
+		}
+	}
+}
+
 // unArchiveFile 解压文件获取目录树
 func (e Engine) unArchiveFile(filepath string) (root *model.DirTree) {
 	filepath = strings.ReplaceAll(filepath, `\`, `/`)
-	// 目录树根
-	root = model.NewDirTree()
-	var walker archiver.Walker
-	if filter.Tar(filepath) {
-		walker = archiver.NewTar()
-	} else if filter.Zip(filepath) || filter.Jar(filepath) {
-		walker = archiver.NewZip()
-	} else if filter.Rar(filepath) {
-		walker = archiver.NewRar()
-	} else if filter.TarGz(filepath) {
-		walker = archiver.NewTarGz()
-	} else if filter.TarBz2(filepath) {
-		walker = archiver.NewTarBz2()
+	src, err := os.Open(filepath)
+	if err != nil {
+		logs.Warn(err)
+		return
 	}
-	if err := walker.Walk(filepath, func(f archiver.File) error {
-		if !f.IsDir() {
-			bar.Archive.Add(1)
-			// 跳过过大文件
-			if f.Size() > 1024*1024*1024 {
-				return nil
-			}
-			fileName := f.Name()
-			if file, ok := f.Header.(zip.FileHeader); ok {
-				if file.Flags == 0 {
-					gbkName := mahonia.NewDecoder("gbk").ConvertString(file.Name)
-					_, cdata, _ := mahonia.NewDecoder("utf-8").Translate([]byte(gbkName), true)
-					fileName = string(cdata)
-				} else {
+	// 拷贝一份文件再处理
+	temp.DoInTempDir(func(tempdir string) {
+		filepath = path.Join(tempdir, path.Base(filepath))
+		dst, err := os.Create(filepath)
+		if err != nil {
+			logs.Warn(err)
+			return
+		}
+		io.Copy(dst, src)
+		checkArchive(filepath)
+		// 目录树
+		root = model.NewDirTree()
+		var walker archiver.Walker
+		if filter.Tar(filepath) {
+			walker = archiver.NewTar()
+		} else if filter.Zip(filepath) || filter.Jar(filepath) {
+			walker = archiver.NewZip()
+		} else if filter.Rar(filepath) {
+			walker = archiver.NewRar()
+		} else if filter.TarGz(filepath) {
+			walker = archiver.NewTarGz()
+		} else if filter.TarBz2(filepath) {
+			walker = archiver.NewTarBz2()
+		}
+		if err := walker.Walk(filepath, func(f archiver.File) error {
+			if !f.IsDir() {
+				bar.Archive.Add(1)
+				// 跳过过大文件
+				if f.Size() > 1024*1024*1024 {
+					return nil
+				}
+				fileName := f.Name()
+				if file, ok := f.Header.(zip.FileHeader); ok {
+					if file.Flags == 0 {
+						gbkName := mahonia.NewDecoder("gbk").ConvertString(file.Name)
+						_, cdata, _ := mahonia.NewDecoder("utf-8").Translate([]byte(gbkName), true)
+						fileName = string(cdata)
+					} else {
+						fileName = file.Name
+					}
+				} else if file, ok := f.Header.(*rardecode.FileHeader); ok {
+					fileName = file.Name
+				} else if file, ok := f.Header.(*gzip.Header); ok {
+					fileName = file.Name
+				} else if file, ok := f.Header.(*tar.Header); ok {
 					fileName = file.Name
 				}
-			} else if file, ok := f.Header.(*rardecode.FileHeader); ok {
-				fileName = file.Name
-			} else if file, ok := f.Header.(*gzip.Header); ok {
-				fileName = file.Name
-			} else if file, ok := f.Header.(*tar.Header); ok {
-				fileName = file.Name
-			}
-			// 读取文件数据
-			data, err := ioutil.ReadAll(f)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			// 格式化路径
-			fileName = strings.ReplaceAll(fileName, `\`, `/`)
-			if e.checkFile(fileName) {
-				// 支持解析的文件
-				root.AddFile(model.NewFileData(fileName, data))
-			} else if filter.AllPkg(fileName) {
-				// 将压缩包解压到本地
-				temp.DoInTempDir(func(tempdir string) {
-					targetPath := path.Join(tempdir, path.Base(fileName))
-					if out, err := os.Create(targetPath); err == nil {
-						_, err = out.Write(data)
-						out.Close()
-						if err != nil {
+				// 读取文件数据
+				data, err := ioutil.ReadAll(f)
+				if err != nil {
+					return errors.WithStack(err)
+				}
+				// 格式化路径
+				fileName = strings.ReplaceAll(fileName, `\`, `/`)
+				if e.checkFile(fileName) {
+					// 支持解析的文件
+					root.AddFile(model.NewFileData(fileName, data))
+				} else if filter.AllPkg(fileName) {
+					// 将压缩包解压到本地
+					temp.DoInTempDir(func(tempdir string) {
+						targetPath := path.Join(tempdir, path.Base(fileName))
+						if out, err := os.Create(targetPath); err == nil {
+							_, err = out.Write(data)
+							out.Close()
+							if err != nil {
+								logs.Error(err)
+							}
+							// 获取当前目录树
+							dir := root.GetDir(fileName)
+							name := path.Base(fileName)
+							if _, ok := dir.SubDir[name]; !ok {
+								// 将压缩包的内容添加到当前目录树
+								dir.DirList = append(dir.DirList, name)
+								dir.SubDir[name] = e.unArchiveFile(targetPath)
+							}
+						} else {
 							logs.Error(err)
 						}
-						// 获取当前目录树
-						dir := root.GetDir(fileName)
-						name := path.Base(fileName)
-						if _, ok := dir.SubDir[name]; !ok {
-							// 将压缩包的内容添加到当前目录树
-							dir.DirList = append(dir.DirList, name)
-							dir.SubDir[name] = e.unArchiveFile(targetPath)
-						}
-					} else {
-						logs.Error(err)
-					}
-				})
+					})
+				}
 			}
+			return nil
+		}); err != nil {
+			logs.Error(errors.WithMessage(err, filepath))
 		}
-		return nil
-	}); err != nil {
-		logs.Error(errors.WithMessage(err, filepath))
-	}
+	})
 	return root
 }
 
