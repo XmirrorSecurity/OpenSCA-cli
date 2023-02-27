dccf6f331d8d2f00c0d9571f1392ca5f9758b74b
luotianqi777 <luotianqi777@gmail.com>
2023-02-27 15:56:26 +0800

    add statistics info

--- cli/main.go
+++ cli/main.go
@@ -59,4 +59,37 @@ func output(depRoot *model.DepTree, taskInfo report.TaskInfo) {
 	} else {
 		fmt.Println(string(reportFunc(depRoot, taskInfo)))
 	}
+	if taskInfo.Error == nil && taskInfo.ErrorString == "" {
+		coms := map[int]int{
+			0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+		}
+		vuls := map[int]int{
+			0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
+		}
+		vset := map[string]bool{}
+		q := []*model.DepTree{depRoot}
+		for len(q) > 0 {
+			n := q[0]
+			risk := 5
+			for _, v := range n.Vulnerabilities {
+				if !vset[v.Id] {
+					vset[v.Id] = true
+					vuls[v.SecurityLevelId]++
+					vuls[0]++
+				}
+				if v.SecurityLevelId < risk {
+					risk = v.SecurityLevelId
+				}
+			}
+			coms[risk]++
+			coms[0]++
+			q = append(q[1:], n.Children...)
+		}
+		fmt.Printf("\nComplete!"+
+			"\nComponents:%d C:%d H:%d M:%d L:%d"+
+			"\nVulnerabilities:%d C:%d H:%d M:%d L:%d\n",
+			coms[0], coms[1], coms[2], coms[3], coms[4],
+			vuls[0], vuls[1], vuls[2], vuls[3], vuls[4],
+		)
+	}
 }
