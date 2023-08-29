fd578eaabb74f39cb7dc923ea64f55f96cc094d5
luotianqi777 <luotianqi777@gmail.com>
2023-08-29 12:00:39 +0800

    php

--- opensca/sca/php/composer.go
+++ opensca/sca/php/composer.go
@@ -23,9 +23,9 @@ type ComposerJson struct {
 }
 
 type ComposerLock struct {
-	Packages []*ComposerLockPackage `json:"packages"`
+	Packages []*ComposerPackage `json:"packages"`
 }
-type ComposerLockPackage struct {
+type ComposerPackage struct {
 	Name       string            `json:"name"`
 	Version    string            `json:"version"`
 	License    []string          `json:"license"`
@@ -34,7 +34,9 @@ type ComposerLockPackage struct {
 }
 
 type ComposerRepo struct {
-	Packages map[string][]*ComposerJson `json:"packages"`
+	Packages struct {
+		Versions map[string]*ComposerPackage `json:"versions"`
+	} `json:"packages"`
 }
 
 func readJson[T any](reader io.Reader) *T {
@@ -51,7 +53,7 @@ func ParseComposerJsonWithLock(json *ComposerJson, lock *ComposerLock) *model.De
 	root := &model.DepGraph{Name: json.Name, Path: json.File.Path()}
 	root.AppendLicense(json.License)
 
-	_dep := model.NewDepGraphMap(func(s ...string) string { return s[0] }, func(s ...string) *model.DepGraph { return &model.DepGraph{Name: s[0]} }).LoadOrStore
+	_dep := model.NewDepGraphMap(nil, func(s ...string) *model.DepGraph { return &model.DepGraph{Name: s[0]} }).LoadOrStore
 
 	// 第一次遍历记录依赖信息
 	for _, pkg := range lock.Packages {
@@ -84,25 +86,87 @@ func ParseComposerJsonWithLock(json *ComposerJson, lock *ComposerLock) *model.De
 	return root
 }
 
-var phpOrigin = func(name, version string) *ComposerJson {
+func ParseComposerJsonWithOrigin(json *ComposerJson) *model.DepGraph {
 
-	var origin *ComposerJson
+	root := &model.DepGraph{Name: json.Name, Path: json.File.Path()}
+	root.AppendLicense(json.License)
 
-	findComposerJsonFromRepo := func(repo *ComposerRepo) *ComposerJson {
+	_dep := model.NewDepGraphMap(nil, func(s ...string) *model.DepGraph { return &model.DepGraph{Name: s[0], Version: s[1]} }).LoadOrStore
+
+	root.Expand = &ComposerPackage{
+		Name:       json.License,
+		Require:    json.Require,
+		RequireDev: json.RequireDev,
+	}
+
+	root.ForEachNode(func(p, n *model.DepGraph) bool {
+
+		pkg := n.Expand.(*ComposerPackage)
+		for _, lic := range pkg.License {
+			n.AppendLicense(lic)
+		}
+
+		for name, version := range pkg.Require {
+
+			subpkg := phpOrigin(name, version)
+
+			if subpkg == nil {
+				n.AppendChild(_dep(name, version))
+				continue
+			}
+
+			dep := _dep(subpkg.Name, subpkg.Version)
+			dep.Expand = subpkg
+			n.AppendChild(dep)
+		}
+
+		for name, version := range pkg.RequireDev {
+
+			subpkg := phpOrigin(name, version)
+
+			if subpkg == nil {
+				dep := _dep(name, version)
+				dep.Develop = true
+				n.AppendChild(dep)
+				continue
+			}
+
+			dep := _dep(subpkg.Name, subpkg.Version)
+			dep.Expand = subpkg
+			dep.Develop = true
+			n.AppendChild(dep)
+		}
+
+		return true
+	})
+
+	root.ForEachNode(func(p, n *model.DepGraph) bool { n.Expand = nil; return true })
+
+	return root
+}
+
+var phpOrigin = func(name, version string) *ComposerPackage {
+
+	var origin *ComposerPackage
+
+	findComposerJsonFromRepo := func(repo ComposerRepo) *ComposerPackage {
 		vers := []string{}
-		for v := range repo.Packages {
+		for v := range repo.Packages.Versions {
 			vers = append(vers, v)
 		}
 		maxv := findMaxVersion(version, vers)
-		origin := repo.Packages[maxv][0]
-		return origin
+		return repo.Packages.Versions[maxv]
 	}
 
 	// 读取缓存
 	path := cache.Path("", name, version, model.Lan_Php)
 	cache.Load(path, func(reader io.Reader) {
-		repo := readJson[ComposerRepo](reader)
-		origin = findComposerJsonFromRepo(repo)
+		var repo ComposerRepo
+		if err := json.NewDecoder(reader).Decode(&repo); err != nil {
+			logs.Warn(err)
+		} else {
+			origin = findComposerJsonFromRepo(repo)
+		}
 	})
 
 	if origin != nil {
@@ -113,26 +177,36 @@ var phpOrigin = func(name, version string) *ComposerJson {
 	url := fmt.Sprintf("https://repo.packagist.org/p2/%s.json", name)
 	if rep, err := http.Get(url); err == nil {
 		defer rep.Body.Close()
+
 		if rep.StatusCode != 200 {
 			logs.Warnf("code:%d url:%s", rep.StatusCode, url)
 			io.Copy(io.Discard, rep.Body)
 		} else {
+
 			logs.Infof("code:%d url:%s", rep.StatusCode, url)
 			data, err := io.ReadAll(rep.Body)
-			if err == nil {
-				reader := bytes.NewReader(data)
-				cache.Save(path, reader)
-				reader.Seek(0, io.SeekStart)
-				repo := readJson[ComposerRepo](reader)
-				origin = findComposerJsonFromRepo(repo)
+			if err != nil {
+				logs.Warn(err)
+				return origin
 			}
+
+			reader := bytes.NewReader(data)
+			var repo ComposerRepo
+			if err := json.NewDecoder(reader).Decode(&repo); err != nil {
+				logs.Warnf("unmarshal json from %s err: %s", url, err)
+				return origin
+			}
+
+			reader.Seek(0, io.SeekStart)
+			cache.Save(path, reader)
+			origin = findComposerJsonFromRepo(repo)
 		}
 	}
 
 	return origin
 }
 
-func RegisterNpmOrigin(origin func(name, version string) *ComposerJson) {
+func RegisterNpmOrigin(origin func(name, version string) *ComposerPackage) {
 	if origin != nil {
 		phpOrigin = origin
 	}
--- opensca/sca/php/sca.go
+++ opensca/sca/php/sca.go
@@ -2,6 +2,9 @@ package php
 
 import (
 	"context"
+	"io"
+	"path"
+	"strings"
 
 	"github.com/xmirrorsecurity/opensca-cli/opensca/model"
 	"github.com/xmirrorsecurity/opensca-cli/opensca/sca/filter"
@@ -18,5 +21,35 @@ func (sca Sca) Filter(relpath string) bool {
 }
 
 func (sca Sca) Sca(ctx context.Context, parent *model.File, files []*model.File) []*model.DepGraph {
-	return nil
+
+	jsonMap := map[string]*ComposerJson{}
+	lockMap := map[string]*ComposerLock{}
+
+	path2dir := func(relpath string) string { return path.Dir(strings.ReplaceAll(relpath, `\`, `/`)) }
+
+	for _, f := range files {
+		if filter.PhpComposer(f.Relpath) {
+			f.OpenReader(func(reader io.Reader) {
+				jsonMap[path2dir(f.Relpath)] = readJson[ComposerJson](reader)
+			})
+		} else if filter.PhpComposerLock(f.Relpath) {
+			f.OpenReader(func(reader io.Reader) {
+				lockMap[path2dir(f.Relpath)] = readJson[ComposerLock](reader)
+			})
+		}
+	}
+
+	var root []*model.DepGraph
+	for dir, json := range jsonMap {
+
+		// 通过lock文件补全
+		if lock, ok := lockMap[dir]; ok {
+			root = append(root, ParseComposerJsonWithLock(json, lock))
+		}
+
+		// 从数据源下载
+		root = append(root, ParseComposerJsonWithOrigin(json))
+	}
+
+	return root
 }
